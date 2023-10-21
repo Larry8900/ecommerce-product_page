037723819fb28841cb3be873183ccb5a6c32702d
larry8900 <quickexchange488@gmail.com>
2023-10-21 23:06:24 +0100

    update

--- product-page/src/App.js
+++ product-page/src/App.js
@@ -4,6 +4,7 @@ import {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import {Nav, Button, NavLink, NavItem} from 'react-bootstrap'
 
+
 function App() {
   return (
     <div className="App container-fluid col-sm-12 col-md-12 col-lg-12 col-xl-12">
@@ -28,26 +29,27 @@ function App() {
           </div>
           
           <NavItem>
-            <span>Cart pic</span>
-            <span>Profile pic</span>
+            <span><img src='./icon-cart.svg'  alt='re'/></span>
+            <span><img src='./image-avatar.png' alt='pro' /></span>
           </NavItem>
 
         </Nav>
         <section className='container col-sm-12 col-md-12 col-lg-12 col-xl-12'>
-        <div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
-          <img  />
-          <img />
-          <img />
-          <img />
-          <img />
-        </div>
-        <div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
-        <h4>SNEAKER COMPANY</h4>
-        <h1>Fall limited Sneakers</h1>
-        <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
-        <h3>$125.00  <span>50%</span></h3>
-        <span></span>
-        </div>
+          <div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+            <img src='./image-product-1.jpg' className='img-fluid' />
+            <img src='./image-product-1-thumbnail.jpg' className='img-fluid'/>
+            <img src='./image-product-2-thumbnail.jpg' className='img-fluid'/>
+            <img src='./image-product-3-thumbnail.jpg' className='img-fluid'/>
+            <img src='./image-product-4-thumbnail.jpg' className='img-fluid'/>
+          </div>
+
+          <div className='col-sm-12 col-md-6 col-lg-6 col-xl-6'>
+            <h4>SNEAKER COMPANY</h4>
+            <h1>Fall limited Sneakers</h1>
+            <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
+            <h3>$125.00  <span>50%</span></h3>
+            <span></span>
+          </div>
         
 
         </section>
