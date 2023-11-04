dbc73818243d76750657503bee282ef4071a860b
larry8900 <quickexchange488@gmail.com>
2023-11-04 23:27:03 +0100

    update

--- product-page/src/Modal.js
+++ product-page/src/Modal.js
@@ -11,72 +11,54 @@ function ShowModal() {
     image: './image-product-1.jpg',
     id: 1
   });
-  const [num,setNum] =useState(1)
-  const [selected, setSelected] = useState(false);
+
   
    const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  const next = () => {
-    setImage((prevState) => {
-      const nextId = prevState.id + 1;
-      const imageId = String(nextId).padStart(2,''); // Format to two digits with leading zeros
-      const imagePath = `./image-product-${imageId}.jpg`;
-      console.log(`Next image ID: ${imageId}`);
-      console.log(`Next image path: ${imagePath}`);
-      return {
-        
+  const next = (image) => {
+    const nextId = image.id + 1;
+    const imageId = String(nextId).padStart(2,'');  // Format to two digits with leading zeros
+    
+    if (imageId >= 1 && imageId <= 4) {
+        const imagePath = `./image-product-${imageId}.jpg`;
+         setImage({
         id: nextId,
         image: imagePath,
-      };
-    });
+      })
+    }
   };
 
   const previous = () => {
-    setImage((prevState) => {
-      const nextId = prevState.id - 1;
-      const imageId = String(nextId).padStart(2,''); // Format to two digits with leading zeros
-      const imagePath = `./image-product-${imageId}.jpg`;
-      console.log(`Next image ID: ${imageId}`);
-      console.log(`Next image path: ${imagePath}`);
-      return {
-        ...prevState,
-        
+    const nextId = image.id - 1;
+    const imageId = String(nextId).padStart(2,''); // Format to two digits with leading zeros
+    
+    if (imageId >= 1 && imageId <= 4) {
+        const imagePath = `./image-product-${imageId}.jpg`;
+        setImage({
+        id: nextId,
         image: imagePath,
-      };
-    });
+      })
+    }
   };
   
 
 
-  
-
- 
-//   const next = ()=> {
-//     setImage(prevState => {
-//       return {...prevState, id: prevState.id + 1,
-//         image:`./image-product-${prevState.id}.jpg` ,
-//       }
-//     })
-//   }
 
   return (
     <>
     <div className='setDiv'>
-        
-        <img src={image} onClick={handleShow} className='img-fluid main-img'/>
-        <img src='./icon-previous.svg' onClick={previous} alt='previous' className='prev_image' />
-        <img src='./icon-next.svg' onClick={next} alt='previous' className='next_image' />
+        <img src={image.image} onClick={handleShow} className='img-fluid main-img'/>
+        <img src='./icon-previous.svg' onClick={() => previous(image)} alt='previous' className='prev_image' />
+        <img src='./icon-next.svg' onClick={() => next(image)} alt='previous' className='next_image' />
     </div>
-    
-    
-    
+        
     <div className='thumbnail'>
 
-        <img src='./image-product-1-thumbnail.jpg' onClick={()=> setImage('./image-product-1.jpg')} className='img-fluid thumb' />
-        <img src='./image-product-2-thumbnail.jpg' onClick={()=> setImage('./image-product-2.jpg')} className='img-fluid thumb'/>
-        <img src='./image-product-3-thumbnail.jpg' onClick={()=> setImage('./image-product-3.jpg')} className='img-fluid thumb'/>
-        <img src='./image-product-4-thumbnail.jpg' onClick={()=> setImage('./image-product-4.jpg')} className='img-fluid thumb'/>
+        <img src='./image-product-1-thumbnail.jpg' onClick={()=> setImage({image: './image-product-1.jpg', id: 1})} className='img-fluid thumb' />
+        <img src='./image-product-2-thumbnail.jpg' onClick={()=> setImage({image: './image-product-2.jpg', id: 2})} className='img-fluid thumb'/>
+        <img src='./image-product-3-thumbnail.jpg' onClick={()=> setImage({image: './image-product-3.jpg', id: 3})} className='img-fluid thumb'/>
+        <img src='./image-product-4-thumbnail.jpg' onClick={()=> setImage({image: './image-product-4.jpg', id: 4})} className='img-fluid thumb'/>
                 
     </div>
       <Modal
@@ -91,16 +73,15 @@ function ShowModal() {
         </Modal.Header>
         <Modal.Body className='modal_body'>
         
-        <img src={image} alt='image' className='img-fluid main_img' />
+        <img src={image.image} alt='image' className='img-fluid main_img' />
         
         </Modal.Body>
         <Modal.Footer  className='thumbnail'>
-        
              
-             <img src='./image-product-1-thumbnail.jpg' onClick={()=> setImage('./image-product-1.jpg')} className='img-fluid thum'/>
-             <img src='./image-product-2-thumbnail.jpg' onClick={()=> setImage('./image-product-2.jpg')} className='img-fluid thum'/>
-             <img src='./image-product-3-thumbnail.jpg' onClick={()=> setImage('./image-product-3.jpg')} className='img-fluid thum'/>
-             <img src='./image-product-4-thumbnail.jpg' onClick={()=> setImage('./image-product-4.jpg')} className='img-fluid thum'/>
+             <img src='./image-product-1-thumbnail.jpg' onClick={()=> setImage({image: './image-product-1.jpg', id: 1})} className='img-fluid thum'/>
+             <img src='./image-product-2-thumbnail.jpg' onClick={()=> setImage({image: './image-product-2.jpg', id: 2})} className='img-fluid thum'/>
+             <img src='./image-product-3-thumbnail.jpg' onClick={()=> setImage({image: './image-product-3.jpg', id: 3})} className='img-fluid thum'/>
+             <img src='./image-product-4-thumbnail.jpg' onClick={()=> setImage({image: './image-product-4.jpg', id: 4})} className='img-fluid thum'/>
            
         </Modal.Footer>
       </Modal>
